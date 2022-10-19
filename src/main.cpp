484a10bcd96a7c1ad0fc5f7eef42064026d24c9e
Fanuel Conrad <59276995+FanuelConrad@users.noreply.github.com>
2022-10-19 22:56:10 +0300

    introduced *Meter3

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,7 @@
 // connect a flow meter to an interrupt pin (see notes on your Arduino model for pin numbers)
 FlowMeter *Meter1;
 FlowMeter *Meter2;
+FlowMeter *Meter3;
 
 // set the measurement update period to 1s (1000 ms)
 const unsigned long period = 1000;
@@ -21,6 +22,11 @@ void Meter2ISR() {
     Meter2->count();
 }
 
+// define an 'interrupt service handler' (ISR) for every interrupt pin you use
+void Meter3ISR() {
+    // let our flow meter count the pulses
+    Meter3->count();
+}
 
 void setup() {
   // prepare serial communication
@@ -31,6 +37,9 @@ void setup() {
     
     // do this setup step for every  FlowMeter and ISR you have defined, depending on how many you need
     Meter2 = new FlowMeter(digitalPinToInterrupt(3), UncalibratedSensor, Meter2ISR, RISING);
+
+    // do this setup step for every  FlowMeter and ISR you have defined, depending on how many you need
+    Meter3 = new FlowMeter(digitalPinToInterrupt(4), UncalibratedSensor, Meter3ISR, RISING);
 }
 
 void loop() {
@@ -40,10 +49,13 @@ void loop() {
     // process the (possibly) counted ticks
     Meter1->tick(period);
     Meter2->tick(period);
+    Meter3->tick(period);
 
     // output some measurement result
     Serial.println("Meter 1 currently " + String(Meter1->getCurrentFlowrate()) + " l/min, " + String(Meter1->getTotalVolume())+ " l total.");
     Serial.println("Meter 2 currently " + String(Meter2->getCurrentFlowrate()) + " l/min, " + String(Meter2->getTotalVolume())+ " l total.");
+    Serial.println("Meter 3 currently " + String(Meter3->getCurrentFlowrate()) + " l/min, " + String(Meter3->getTotalVolume())+ " l total.");
+
 
 
 }
