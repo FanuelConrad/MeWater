50eab80e82b8bccdacec80a93e381bf376ffee60
Fanuel Conrad <59276995+FanuelConrad@users.noreply.github.com>
2022-10-28 20:38:30 +0300

    updated LeakageDetection.h

---
+++ include/leakage_detection/LeakageDetection.h
@@ -0,0 +1,21 @@
+#ifndef LEAKAGEDETECTION_H
+#define LEAKAGEDETECTION_H
+
+#include <Arduino.h>
+
+class LeakageDetection{
+    private: float initial_flowrate;
+    private: float normal_flowrate;
+    private: float leak_flowrate;
+    private: float normal_velocity;
+    private: float leak_velocity;
+    private: float acceleration;
+    private: float leak_location;
+
+    void Calculate_normal_velocity();
+    void Calculate_leak_velocity();
+    void Calculate_water_acceleration();
+    void Calculate_location_of_leak();
+};
+
+#endif
